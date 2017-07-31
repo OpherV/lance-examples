865170795c86c4a0d417292188d5689a9fa65291
Opher Vishnia <opherv@gmail.com>
2017-07-31 16:31:45 +0300

    No need to set class on Rect

--- examples/hshg/Rect.js
+++ examples/hshg/Rect.js
@@ -1,10 +1,3 @@
 import DynamicObject from 'lance/serialize/DynamicObject'
 
-export default class Circle extends DynamicObject {
-
-    constructor(id, position) {
-        super(id, position);
-        this.class = Circle;
-    };
-
-}
+export default class Circle extends DynamicObject {}
